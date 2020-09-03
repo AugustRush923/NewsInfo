d22573df4047e6c0cd432e1609c1835e71e2402c
AugustRush923 <augustrush923@163.com>
2020-09-03 14:49:12 +0800

    静态文件的导入以及注册根路由

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,6 +1,12 @@
 from . import index_blu
+from flask import render_template, current_app
 
 
-@index_blu.route('/index')
+@index_blu.route('/')
 def index():
-    return 'index'
+    return render_template("news/index.html")
+
+
+@index_blu.route('/favicon.ico')
+def favicon():
+    return current_app.send_static_file('news/favicon.ico')
