7310f90ffc6ef98bddeb4297362ed679366a7082
AugustRush923 <augustrush923@163.com>
2020-09-07 11:13:02 +0800

    首页"新闻列表"数据获取及展示功能的实现

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,8 +1,9 @@
 from . import index_blu
-from flask import render_template, current_app, session
+from flask import render_template, current_app, session, request, jsonify
 
 from info.models import News, User, Category
 from info import constants
+from info.utils.response_code import RET
 
 
 @index_blu.route('/')
@@ -48,3 +49,56 @@ def index():
 @index_blu.route('/favicon.ico')
 def favicon():
     return current_app.send_static_file('news/favicon.ico')
+
+
+@index_blu.route("/news_list")
+def new_list():
+    """
+       获取指定分类的新闻列表
+       1. 获取参数
+       2. 校验参数
+       3. 查询数据
+       4. 返回数据
+       :return:
+   """
+    # 获取参数
+    cid = request.args.get("cid")
+    page = request.args.get('page', 1)
+    per_page = request.args.get("per_page", constants.HOME_PAGE_MAX_NEWS)
+
+    # 校验参数
+    try:
+        cid = int(cid)
+        page = int(page)
+        per_page = int(per_page)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数")
+    filters = [News.status == 0]
+    if cid != 1:
+        filters.append(News.category_id == cid)
+    print(filters)
+
+    # 查询数据
+    try:
+        paginate = News.query.filter(*filters).order_by(News.create_time.desc()).paginate(page, per_page, False)
+        # 获取查询出来的数据
+        items = paginate.items
+        # 获取到总页数
+        total_page = paginate.pages
+        current_page = paginate.page
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据查询错误")
+
+    news_li = []
+    for news in items:
+        news_li.append(news.to_basic_dict())
+
+    print(news_li)
+    data = {
+        "total_page": total_page,
+        "current_page": current_page,
+        "news_dict_li": news_li
+    }
+    return jsonify(errno=RET.OK, errmsg="OK", data=data)
